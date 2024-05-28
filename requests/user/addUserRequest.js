ae434c7b3c115cb046587f156b016be149ebce67
shamna-parveen <shamna@techfriar.com>
2024-05-28 10:50:23 +0400

    add

--- requests/user/addUserRequest.js
+++ requests/user/addUserRequest.js
@@ -1,72 +1,62 @@
-import Joi from 'joi'
-import userRepository from "../../repositories/userRepository.js"
-import bcryptPassword from  "../../utils/bcryptPassword.js"
+import Joi from "joi";
+import userRepository from "../../repositories/userRepository.js";
 class AddEmployeeRequest {
-    static employeeRepo = new userRepository()
+  static employeeRepo = new userRepository();
 
+  /**
+   * Add validation rules for the request
+   */
+  static schema = Joi.object({
+    name: Joi.string().required().messages({
+      "string.empty": "Please enter the full name of the employee.",
+      "any.required": "Please enter the full name of the employee.",
+    }),
+    email: Joi.string().email().required().messages({
+      "string.empty": "Please enter a valid email address.",
+      "any.required": "Please enter a valid email address.",
+      "string.email": "Please enter a valid email address.",
+    }),
+    password: Joi.string().required().messages({
+      "string.empty": "Password is required.",
+      "any.required": "Password is required.",
+    }),
+  });
+
+  constructor(req) {
+    this.data = {
+      name: req.query.name,
+      email: req.query.email,
+      password: req.query.password,
+    };
+  }
+
+  async validate() { 
+    const { error, value } = AddEmployeeRequest.schema.validate(this.data, {
+      abortEarly: false,
+    });
     /**
-     * Add validation rules for the request
+     * Check email exist or not
      */
-    static schema = Joi.object({
-        name: Joi.string().required().messages({
-            'string.empty': 'Please enter the full name of the employee.',
-            'any.required': 'Please enter the full name of the employee.',
-        }),
-        email: Joi.string().email().required().messages({
-            'string.empty': 'Please enter a valid email address.',
-            'any.required': 'Please enter a valid email address.',
-            'string.email': 'Please enter a valid email address.',
-        }),
-        password: Joi.string().required().messages({
-            'string.empty': 'Password is required.',
-            'any.required': 'Password is required.',
-          }),
-        
-    })
+    const checkEmailExists =
+      await AddEmployeeRequest.employeeRepo.getEmployeeByEmail(this.data.email);
 
-    constructor(req) {
-        this.data = {
-            name: req.query.name,
-            email: req.query.email,
-            password:req.query.password
-        }
+    if (error || checkEmailExists) {
+      const validationErrors = {};
+      error
+        ? error.details.forEach((err) => {
+            validationErrors[err.context.key] = err.message;
+          })
+        : [];
+
+      if (checkEmailExists !== null) {
+        validationErrors["email"] =
+          "Email id is already taken. Try another one.";
+      }
+
+      throw validationErrors;
     }
-
-    async validate() {
-        const { error, value } = AddEmployeeRequest.schema.validate(this.data, {
-            abortEarly: false,
-        })
-        /**
-         * Check email exist or not
-         */
-        const checkEmailExists =
-            await AddEmployeeRequest.employeeRepo.getEmployeeByEmail(
-                this.data.email,
-            )
-
-
-        if (
-            error ||
-            checkEmailExists 
-           
-        ) {
-            const validationErrors = {}
-            error
-                ? error.details.forEach((err) => {
-                      validationErrors[err.context.key] = err.message
-                  })
-                : []
-         
-            if (checkEmailExists !== null) {
-                validationErrors['email'] =
-                    'Email id is already taken. Try another one.'
-            }
-           
-            
-            throw validationErrors
-        }
-        return value
-    }
+    return value;
+  }
 }
 
-export default AddEmployeeRequest
+export default AddEmployeeRequest;
